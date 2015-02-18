57d16abc20e78e7ae48c02169a2c0ab16ef26830
Tyler Craft <tylerc@fb.com>
2015-02-18 15:01:31 -0800

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Tyler Craft on 2/18/15.
+//  Copyright (c) 2015 Tyler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
